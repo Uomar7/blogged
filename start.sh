8a27ebe8e20e21f4b4029bac9542f734f9f4533c
Uomar <uomarearlie7@gmail.com>
2019-06-03 15:36:05 +0300

    created the request function

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 export SECRET_KEY='umarngare'
 export DATABASE_URL='postgresql+psycopg2://king:Uomarearlie1995localhost/blog1'
+export QUOTE_API_BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
 export MAIL_USERNAME='uomarearlie7@gmail.com'
 export MAIL_PASSWORD='umarngare'
 python3 manage.py server
